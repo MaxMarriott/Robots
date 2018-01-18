ed52ac2f8ffac441afd4e08dacc83e8f5eecc56e
MaxMarriott <33951883+MaxMarriott@users.noreply.github.com>
2018-01-18 21:11:43 +1030

    Update RobotVision.cpp

--- RobotVision.cpp
+++ RobotVision.cpp
@@ -1,11 +1,11 @@
-//#include required things
+oo//#include required things
 #include <iostream>
 #include <GripPipeline.cpp>
 #include <GripPipeline.h>
 
 //all referances to the white object in the input will be refered to as simply "Object", for simplicity's sake
 
-void main() 
+void main()
 {
 	int DummyTurnLeft; //Simulates turning left
 
@@ -13,33 +13,29 @@ void main()
 
 	int DummyDriveForwards; //Simulates driving forward
 
-	int DummyPickupObject; //Simulates block getting picked up
-
-	int DummyLaserSensor; //Simulates the successful pickup of a block
-
+	bool DummyLaserSensor; //Simulates the successful pickup of a block
 	int ObjectMinPrev; //creates a copy of the max of Y from the previous frame
 
-		FindObjectCentre(); 
+	findObjectCentre();
 } //Declares variables for the program.
 
-void FindObjectCentre()
+void findObjectCentre()
 {
 	//DECLERATIONS + FINDING WHITE IN INPUT IMAGE!!!
 		int ObjectMinX = 0;
-		
 		ObjectMinX(0);
 
 		int ObjectMaxX = 0;
 
-		ObjectMaxX(0);
+		objectMaxX(0);
 
-		int ObjectMinY = 0; 
+		int ObjectMinY = 0;
 
 		ObjectMinY(0);
 
 		int ObjectMaxY = 0;
 
-		ObjectMaxY(0);
+		objectMaxY(0);
 
 		//int ObjectMaxY = First white pixel with the highest Y value
 
@@ -50,8 +46,7 @@ void FindObjectCentre()
 
 		//Bool IsObjectPresent = true; //True if there are no white pixels from the input.
 
-		If ((ObjectMaxX = 0) || (ObjectMaxY = 0))
-		{
+		If ((ObjectMaxX = 0) || (ObjectMaxY = 0))		{
 			FindObjectCentre();
 		}
 		else
@@ -63,19 +58,17 @@ void FindObjectCentre()
 				ObjectCentre[1] = (ObjectMaxY + ObjectMinY) / 2; //Y value calculated
 
 				//NON IMPORTANT THINGS HERE
-					std::cout << "X value of the centre: " << ObjectCentre[0] << endl;
+					std::cout << "X value of the centre: " << ObjectCentre[0] << std::endl;
 
-					std::cout << "Y value of the centre: " << ObjectCentre[1] << endl; //too see if program has run
+					std::cout << "Y value of the centre: " << ObjectCentre[1] << std::endl; //too see if program has run
 
 				//Switch to driving phase
 
-					SteerToObject();
+					steerToObject();
 		}
-
-	return 0
 }
 
-void SteerToObject()
+void steerToObject()
 {
 	While(int ObjectCentre[0] > 75) //moves left until approximately at the centre
 	{
@@ -84,16 +77,15 @@ void SteerToObject()
 		FindObjectCentre();
 	}
 
-	DummyTurnLeft = 0; //stops turning left 
+	DummyTurnLeft = 0; //stops turning left
 
 	While(ObjectCentre[0] < 65) //moves right until approximately at the centre
 	{
 		DummyTurnRight = 1;
 
-		FindObjectCentre();
-	}
+		FindObjectCentre();	}
 
-	DummyTurnRight = 0; //Stops turning right 
+	DummyTurnRight = 0; //Stops turning right
 
 	if ((ObjectCentre[0] > 75) || (ObjectCentre[0] < 65))			//to ensure the robot did not drive to far to the right or left!
 	{
@@ -105,7 +97,7 @@ void SteerToObject()
 	}
 }
 
-void PickUpObject()
+void pickUpObject()
 {
 
 	ObjectMinYPrev = ObjectMinY; //creates a copy of the max of Y from the previous frame
@@ -116,7 +108,8 @@ void PickUpObject()
 
 	}
 
-	DummyPickup = 1;
+	Ready; //Pickup
+
 	while (DummyLaserSensor == false)
 	{
 		DummyDriveForwards = 0;
@@ -130,26 +123,26 @@ void PickUpObject()
 
 
 //FUNCTIONS FINDING THE OBJECT'S MIN and Max Values
-int FindObjectMinX (int ObjectMinX)
+int findObjectMinX (int ObjectMinX)
 {
-	//int ObjectMinX = First white pixel with the lowest X value 
+	//int ObjectMinX = First white pixel with the lowest X value
 }
 
 
 
-int FindObjectMaxX (int ObjectMaxX)
+int findObjectMaxX (int ObjectMaxX)
 {
 	//int ObjectMaxX = First white pixel with the highest X value
 }
 
 
-int FindObjectMinY (int ObjectMinY)
+int findObjectMinY (int ObjectMinY)
 {
 	//int ObjectMinY = First white pixel with the lowest Y value
 }
 
 
-int FindObjectMaxY (int ObjectMaxY)
+int findObjectMaxY (int ObjectMaxY)
 {
 	//int ObjectMaxY = First white pixel with the highest Y value
 }
